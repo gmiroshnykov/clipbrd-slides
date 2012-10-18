d9271d315a62854af97735536755e221a62ea390
George Miroshnykov <george.miroshnykov@gmail.com>
2012-10-18 18:16:14 +0300

    upload.sh: --delete-removed

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-s3cmd sync -P --recursive --rexclude='\.git|upload.sh' . s3://clipbrd-slides
+s3cmd sync -P --recursive --delete-removed --rexclude='\.git|upload.sh' . s3://clipbrd-slides
